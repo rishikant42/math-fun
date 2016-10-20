09e25b1f5ad15d5e9c3e68fbe403b58eb2b9a562
rishikant42 <rksbtp@gmail.com>
2016-10-20 20:48:56 +0530

    added test cases

--- test/math_fun/core_test.clj
+++ test/math_fun/core_test.clj
@@ -1,7 +1,34 @@
 (ns math-fun.core-test
   (:require [clojure.test :refer :all]
+            [math-fun.basic_math :refer [sqrt cube-root prime? fact exp sum mult div gcd avg fib]]
             [math-fun.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest exp-test
+  (testing "exponential procedure"
+    (is (= (exp 2 5) 32))))
+
+(deftest gcd-test
+  (testing "gcd procedure"
+    (is (= (gcd 104 20) 4))))
+
+(deftest isprime-test
+  (testing "isprime procedure"
+    (is (= (prime? 1) false))))
+
+(deftest multiple-test
+  (testing "square root procedure"
+    (is (= (sqrt 5) 2.2360688956433634)))
+  (testing "cube root procedure"
+    (is (= (cube-root 5) 1.7099768958229076)))
+  (testing "factorial procedure"
+    (is (= (fact 5) 120)))
+  (testing "sum procedure"
+    (is (= (sum '(1 2 3)) 6)))
+  (testing "multiply procedure"
+    (is (= (mult '(1 2 3)) 6)))
+  (testing "division procedure"
+    (is (= (div '(1 2 3)) (/ 1 6))))
+  (testing "average procedure"
+    (is (= (avg '(1 2 3)) 2.0)))
+  (testing "fibnocci procedure"
+    (is (= (fib 5) 5))))
