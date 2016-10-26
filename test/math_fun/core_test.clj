d0f1b3830da82af3968e947ee38bfac1e5e487ce
rishikant42 <rksbtp@gmail.com>
2016-10-27 00:08:15 +0530

    added test case for series sum

--- test/math_fun/core_test.clj
+++ test/math_fun/core_test.clj
@@ -1,6 +1,7 @@
 (ns math-fun.core-test
   (:require [clojure.test :refer :all]
             [math-fun.basic_math :refer [sqrt cube-root prime? fact exp sum mult div gcd avg fib]]
+            [math-fun.series_sum :refer :all]
             [math-fun.core :refer :all]))
 
 (deftest exp-test
@@ -32,3 +33,8 @@
     (is (= (avg '(1 2 3)) 2.0)))
   (testing "fibnocci procedure"
     (is (= (fib 5) 5))))
+
+
+(deftest series-test
+  (testing "series sum procedure"
+    (is (= (add 3 1 inc 5) 225))))
